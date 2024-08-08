79d61fc60cc1a6e53f49ceed6aba8434941d4ba3
MrH008 <hazmsaleh35@gmail.com>
2024-08-08 10:58:56 +0300

    Update Two.cs

--- Assets/Script/Two.cs
+++ Assets/Script/Two.cs
@@ -7,7 +7,7 @@ public class Two : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int x;
     }
 
     // Update is called once per frame
